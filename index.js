28575a8b9dc735bb6e450328135f973d9baff89b
Tenten Ponce <poncetenten10@gmail.com>
2019-06-08 01:00:58 +0800

    Added bubble graph

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const ParameterService = require('./services/ParameterService')
 const KoinGraphService = require('./services/KoinGraphService')
 const LinkNodeGraphService = require('./services/LinkNodeGraphService')
 const TreeGraphService = require('./services/TreeGraphService')
+const BubbleGraphService = require('./services/BubbleGraphService')
 
 /* get path and graph format */
 let paramService = new ParameterService()
@@ -16,19 +17,33 @@ let koinGraphService = new KoinGraphService(param)
 let graph = koinGraphService.buildGraph()
 
 /* show ui graph */
-if (param.graph == 'tree') {
-  let treeGraphService = new TreeGraphService(graph)
+switch (param.graph) {
+  case 'tree':
+    let treeGraphService = new TreeGraphService(graph)
 
-  let tree = treeGraphService.build()
-  fs.writeFileSync('ui/koin-graph.js', 'var treeData = ' + JSON.stringify(tree, null, 2))
-  opn(path.resolve('ui/tree-graph.html'))
-} else {
-  let linkNodeGraphService = new LinkNodeGraphService(graph)
+    let tree = treeGraphService.build()
+    fs.writeFileSync('ui/koin-graph.js', 'var treeData = ' + JSON.stringify(tree, null, 2))
+    opn(path.resolve('ui/tree-graph.html'))
+    break
+  case 'bubble':
+    let bubbleGraphService = new BubbleGraphService(graph)
 
-  let linkNodeGraph = linkNodeGraphService.build()
+    let bubble = bubbleGraphService.build()
+    fs.writeFileSync('ui/koin-graph.js', 'var bubbleData = ' + JSON.stringify(bubble, null, 2))
+    opn(path.resolve('ui/bubble-graph.html'))
+    break
+  case 'link-node':
+    let linkNodeGraphService = new LinkNodeGraphService(graph)
 
-  fs.writeFileSync('ui/koin-graph.js', '')
-  fs.appendFileSync('ui/koin-graph.js', 'var nodes = ' + JSON.stringify(linkNodeGraph.nodes, null, 2))
-  fs.appendFileSync('ui/koin-graph.js', '\nvar links = ' + JSON.stringify(linkNodeGraph.links, null, 2))
-  opn(path.resolve('ui/link-node-graph.html'))
+    let linkNodeGraph = linkNodeGraphService.build()
+
+    fs.writeFileSync('ui/koin-graph.js', '')
+    fs.appendFileSync('ui/koin-graph.js', 'var nodes = ' + JSON.stringify(linkNodeGraph.nodes, null, 2))
+    fs.appendFileSync('ui/koin-graph.js', '\nvar links = ' + JSON.stringify(linkNodeGraph.links, null, 2))
+    opn(path.resolve('ui/link-node-graph.html'))
+    break
+  default:
+    console.log('Please specify graph by using --graph parameter')
+    console.log('options: bubble, tree, link-node')
+    break
 }
---
+++ services/BubbleGraphService.js
@@ -0,0 +1,101 @@
+class BubbleGraphService {
+
+  constructor(graph) {
+    this.graph = graph
+  }
+
+  build() {
+    let rootModules = this.getRootModules()
+
+    let bubble = {
+      name: 'Dependencies',
+      children: []
+    }
+
+    /**
+     * add roots
+     */
+    for (var i in rootModules) {
+      let moduleName = rootModules[i]
+
+      bubble.children.push({
+        name: moduleName,
+        children: []
+      })
+    }
+
+    // get children of roots, and children of children...
+    for (var i in bubble.children) {
+      let child = bubble.children[i]
+
+      child.children = this.getChildren(child.name)
+    }
+
+    return bubble
+  }
+
+  /**
+   * get children of children
+   * @param moduleName module to check its dependencies
+   */
+  getChildren(moduleName) {
+    let children = []
+
+    let dependencies = this.graph[moduleName]
+    for (var i in dependencies) {
+      let dependency = dependencies[i]
+
+      let child = {
+        name: dependency,
+        children: this.getChildren(dependency),
+        size: this.getChildren(dependency).length * 10
+      }
+
+      /**
+       * if no child, remove children node
+       */
+      if (child.children.length <= 0) {
+        child = {
+          name: dependency,
+          size: 10
+        }
+      }
+
+      children.push(child)
+    }
+
+    return children
+  }
+
+  /**
+   * get all modules that is not a dependency (it means that is a root)
+   */
+  getRootModules() {
+    let rootModules = []
+
+    for (var i in this.graph) {
+      if (!this.isDependency(i)) {
+        rootModules.push(i)
+      }
+    }
+
+    return rootModules
+  }
+
+  isDependency(moduleName) {
+    for (var i in this.graph) {
+      let dependencies = this.graph[i]
+      for (var j in dependencies) {
+        let dependency = dependencies[j]
+
+        if (dependency == moduleName) {
+          return true
+        }
+      }
+    }
+
+    return false
+  }
+}
+
+module.exports = BubbleGraphService
