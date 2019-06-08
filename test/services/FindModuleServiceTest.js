3ee3adb81a6c7368243e9aa8afec26ac8e1110d6
Tenten Ponce <poncetenten10@gmail.com>
2019-06-08 09:48:09 +0800

    Added test on reading class dependency

--- test/services/FindModuleServiceTest.js
+++ test/services/FindModuleServiceTest.js
@@ -8,22 +8,50 @@ const service = new FindModuleService()
 describe('getModules()', () => {
   it('should return modules with dependencies', () => {
     let fileContent = `
-      single { componentA(get(), get()) }
-      single { componentB(get(), get(), get(), get()) }
-      viewModel { viewModelA(get(), get(), get(), get()) }
+      single { ComponentA(get(), get()) }
+      single { ComponentB(get(), get(), get(), get()) }
+      viewModel { ViewModelA(get(), get(), get(), get()) }
     `
 
-    expect(service.getModules(fileContent)).to.include.members(['componentA', 'componentB', 'viewModelA'])
+    expect(service.getModules(fileContent)).to.include.members(['ComponentA', 'ComponentB', 'ViewModelA'])
   })
 
   it('should return modules without dependencies', () => {
     let fileContent = `
-      single { componentA(get(), get()) }
-      single { componentB() }
-      viewModel { viewModelA(get(), get(), get(), get()) }
-      viewModel { viewModelB() }
+      single { ComponentA(get(), get()) }
+      single { ComponentB() }
+      viewModel { ViewModelA(get(), get(), get(), get()) }
+      viewModel { ViewModelB() }
     `
 
-    expect(service.getModules(fileContent)).to.include.members(['componentA', 'componentB', 'viewModelA', 'viewModelB'])
+    expect(service.getModules(fileContent)).to.include.members(['ComponentA', 'ComponentB', 'ViewModelA', 'ViewModelB'])
+  })
+})
+
+describe('getClassDependencies()', () => {
+  it('should return dependencies of the class', () => {
+    let fileContent = `
+      import blah.blah.blah
+      import com.android.blah
+
+      class ComponentA(dependencyA: DependencyA, dependencyB: DependencyB) {
+        more codesss
+      }
+    `
+
+    expect(service.getClassDependencies(fileContent)).to.include.members(['DependencyA', 'DependencyB'])
+  })
+
+  it('should return empty array of dependencies if class has none', () => {
+    let fileContent = `
+      import blah.blah.blah
+      import com.android.blah
+
+      class ComponentA {
+        more codesss
+      }
+    `
+
+    expect(service.getClassDependencies(fileContent)).to.deep.equal([])
   })
 })
