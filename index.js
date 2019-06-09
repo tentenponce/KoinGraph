22fb358b899798688b74fe1ef8674bf407417160
Tenten Ponce <poncetenten10@gmail.com>
2019-06-09 12:12:12 +0800

    Added interactive CLI

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const opn = require('opn')
 const path = require('path')
 const fileHound = require('filehound')
+const inquirer = require('inquirer')
+const chalk = require('chalk')
 
 const FileSystemBridge = require('./data/FileSystemBridge')
 
@@ -13,54 +15,59 @@ const LinkNodeGraphMapper = require('./ui/linknode/LinkNodeGraphMapper')
 const TreeGraphMapper = require('./ui/tree/TreeGraphMapper')
 const BubbleGraphMapper = require('./ui/bubble/BubbleGraphMapper')
 
-/* read parameters */
+/* load the project files */
 let args = require('minimist')(process.argv.slice(2))
-
-let projectPath = args['path']
-let graph = args['graph']
-
-/**
- * get all kotlin files from the project path.
- */
-let projectFiles = fileHound.create()
-  .paths(projectPath)
+console.log(chalk.blue('Reading Project...'))
+let files = fileHound.create()
+  .paths(args['path'])
   .ext('kt')
   .findSync()
+  console.log(chalk.green('Project Loaded'))
 
-/* build json graph */
+/* build koin dependencies json graph */
+console.log(chalk.blue('Reading Koin Dependencies...'))
 let koinGraphService = new KoinGraphService(new FileSystemBridge(),
   new DependencyReaderHelper(),
   new ClassCastHelper())
-let jsonGraph = koinGraphService.buildGraph(projectFiles)
+let jsonGraph = koinGraphService.buildGraph(files)
+console.log(chalk.green('Success building Koin dependencies'))
 
-/* show ui graph */
-switch (graph) {
-  case 'tree':
-    let treeGraphMapper = new TreeGraphMapper(jsonGraph)
+/* ask for type of graph to render */
+inquirer.prompt([{
+  type: "list",
+  name: "graph",
+  message: "Now, what graph do you want to render?",
+  choices: ["Tree Graph", "Bubble Graph", "Link Node Graph"]
+}]).then(answers => {
+  /* show ui graph */
+  switch (answers.graph) {
+    case 'Tree Graph':
+      let treeGraphMapper = new TreeGraphMapper(jsonGraph)
 
-    let tree = treeGraphMapper.toTreeGraph()
-    fs.writeFileSync('ui/tree/koin-graph.js', 'var treeData = ' + JSON.stringify(tree, null, 2))
-    opn(path.resolve('ui/tree/tree-graph.html'))
-    break
-  case 'bubble':
-    let bubbleGraphMapper = new BubbleGraphMapper(jsonGraph)
+      let tree = treeGraphMapper.toTreeGraph()
+      fs.writeFileSync('ui/tree/koin-graph.js', 'var treeData = ' + JSON.stringify(tree, null, 2))
+      opn(path.resolve('ui/tree/tree-graph.html'))
+      break
+    case 'Bubble Graph':
+      let bubbleGraphMapper = new BubbleGraphMapper(jsonGraph)
 
-    let bubble = bubbleGraphMapper.toBubbleGraph()
-    fs.writeFileSync('ui/bubble/koin-graph.js', 'var bubbleData = ' + JSON.stringify(bubble, null, 2))
-    opn(path.resolve('ui/bubble/bubble-graph.html'))
-    break
-  case 'link-node':
-    let linkNodeGraphMapper = new LinkNodeGraphMapper(jsonGraph)
+      let bubble = bubbleGraphMapper.toBubbleGraph()
+      fs.writeFileSync('ui/bubble/koin-graph.js', 'var bubbleData = ' + JSON.stringify(bubble, null, 2))
+      opn(path.resolve('ui/bubble/bubble-graph.html'))
+      break
+    case 'Link Node Graph':
+      let linkNodeGraphMapper = new LinkNodeGraphMapper(jsonGraph)
 
-    let linkNodeGraph = linkNodeGraphMapper.toLinkNodeGraph()
+      let linkNodeGraph = linkNodeGraphMapper.toLinkNodeGraph()
 
-    fs.writeFileSync('ui/linknode/koin-graph.js', '')
-    fs.appendFileSync('ui/linknode/koin-graph.js', 'var nodes = ' + JSON.stringify(linkNodeGraph.nodes, null, 2))
-    fs.appendFileSync('ui/linknode/koin-graph.js', '\nvar links = ' + JSON.stringify(linkNodeGraph.links, null, 2))
-    opn(path.resolve('ui/linknode/link-node-graph.html'))
-    break
-  default:
-    console.log('Please specify graph by using --graph parameter')
-    console.log('options: bubble, tree, link-node')
-    break
-}
+      fs.writeFileSync('ui/linknode/koin-graph.js', '')
+      fs.appendFileSync('ui/linknode/koin-graph.js', 'var nodes = ' + JSON.stringify(linkNodeGraph.nodes, null, 2))
+      fs.appendFileSync('ui/linknode/koin-graph.js', '\nvar links = ' + JSON.stringify(linkNodeGraph.links, null, 2))
+      opn(path.resolve('ui/linknode/link-node-graph.html'))
+      break
+    default:
+      console.log('Please specify graph by using --graph parameter')
+      console.log('options: bubble, tree, link-node')
+      break
+  }
+})
